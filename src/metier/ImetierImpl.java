b955bba570095e9355bf93a28567b81c565bb83e
Youssef Ait Said <saidyoussef642@gmail.com>
2025-10-03 23:49:05 +0100

    First Commit

---
+++ src/metier/ImetierImpl.java
@@ -0,0 +1,12 @@
+package metier;
+import dao.IDao;
+
+public class ImetierImpl implements IMetier {
+
+    private IDao dao;
+
+    @Override
+    public double calcul() {
+        return 0;
+    }
+}
