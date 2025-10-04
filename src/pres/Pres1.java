9ce0ccdca9587f9ddfd0264aed824bb8b4937227
Youssef Ait Said <saidyoussef642@gmail.com>
2025-10-04 01:11:05 +0100

    Version Base de donnes

--- src/pres/Pres1.java
+++ src/pres/Pres1.java
@@ -1,4 +1,15 @@
 package pres;
 
+import dao.DaoImpl;
+import dao.IDao;
+import metier.IMetier;
+import metier.ImetierImpl;
+
 public class Pres1 {
+    public static void main(String[] args) {
+        IDao dao = new DaoImpl();
+        IMetier metier = new ImetierImpl(dao);
+
+        System.out.println("Resultat :" + metier.calcul());
+    }
 }
