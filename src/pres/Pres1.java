5878eee51c53ff3b8fdf5388ef0b154e1b7462dc
Youssef Ait Said <saidyoussef642@gmail.com>
2025-10-04 17:04:01 +0100

    Another Commit

--- src/pres/Pres1.java
+++ src/pres/Pres1.java
@@ -1,13 +1,14 @@
 package pres;
 
 import dao.DaoImpl;
+import dao.DaoImplV2;
 import dao.IDao;
 import metier.IMetier;
 import metier.ImetierImpl;
 
 public class Pres1 {
     public static void main(String[] args) {
-        IDao dao = new DaoImpl();
+        IDao dao = new DaoImplV2();
         IMetier metier = new ImetierImpl(dao);
 
         System.out.println("Resultat :" + metier.calcul());
